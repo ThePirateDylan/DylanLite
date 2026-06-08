2c9eb6ae1a6069c29b0756ca53474041b08a521c
Dylan <selenskydylan@gmail.com>
2026-06-08 09:58:27 -0400

    Updates to The Pirate
    ! Changes to The Pirate Streaming
    ---------------------------------------
    - All UI label to Fen Light have been replaced with The Pirate
    - All Authorizations have been changed to say The Pirate
    ____________________________________
    
    ! Changes to The Pirate Skin
    --------------------------------
    - Changes to UI Color
    _______________________________

--- repo/plugin.video.pirate/resources/lib/service.py
+++ repo/plugin.video.pirate/resources/lib/service.py
@@ -17,7 +17,7 @@ update_string = 'Next Update in %s minutes...'
 
 class SetAddonConstants:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'SetAddonConstants Service Starting')
+		kodi_utils.logger('The Pirate', 'SetAddonConstants Service Starting')
 		import random
 		addon_items = [
 			('fenlight.playback_key', str(random.randint(1000, 10000))),
@@ -30,31 +30,31 @@ class SetAddonConstants:
 			('fenlight.addon_fanart', kodi_utils.addon_fanart())
 					]
 		for item in addon_items: kodi_utils.set_property(*item)
-		return kodi_utils.logger('Fen Light', 'SetAddonConstants Service Finished')
+		return kodi_utils.logger('The Pirate', 'SetAddonConstants Service Finished')
 
 class DatabaseMaintenance:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'DatabaseMaintenance Service Starting')
+		kodi_utils.logger('The Pirate', 'DatabaseMaintenance Service Starting')
 		from caches.base_cache import check_databases_integrity
 		check_databases_integrity(silent=True)
-		return kodi_utils.logger('Fen Light', 'DatabaseMaintenance Service Finished')
+		return kodi_utils.logger('The Pirate', 'DatabaseMaintenance Service Finished')
 
 class SyncSettings:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'SyncSettings Service Starting')
+		kodi_utils.logger('The Pirate', 'SyncSettings Service Starting')
 		sync_settings()
-		return kodi_utils.logger('Fen Light', 'SyncSettings Service Finished')
+		return kodi_utils.logger('The Pirate', 'SyncSettings Service Finished')
 
 class OnUpdateChanges:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'OnUpdateChanges Service Starting')
+		kodi_utils.logger('The Pirate', 'OnUpdateChanges Service Starting')
 		try:
 			for method in list(filter(lambda x: x[0] != 'run', inspect.getmembers(OnUpdateChanges, predicate=inspect.isfunction))):
 				if not get_setting('fenlight.updatechecks.%s' % method[0], 'false') == 'true':
 					method[1](self)
 					set_setting('updatechecks.%s' % method[0], 'true')
 		except: pass
-		return kodi_utils.logger('Fen Light', 'OnUpdateChanges Service Finished')
+		return kodi_utils.logger('The Pirate', 'OnUpdateChanges Service Finished')
 
 	def refresh_addon_keys(self):
 		# For update 2.2.01 - 03
@@ -82,16 +82,16 @@ class OnUpdateChanges:
 			clear_all_trakt_cache_data(silent=True, refresh=False)
 			show_dialog = True
 		if show_dialog:
-			text = 'The original developer of Fen Light has revoked all keys (Trakt, TMDb) that have been used up until now '\
+			text = 'The original developer of The Pirate has revoked all keys (Trakt, TMDb) that have been used up until now '\
 			'within this addon, with permission, due to security concerns.'
 			kodi_utils.ok_dialog(heading='Addon Credentials Reset', text=text)
-			text = 'This unfortunately means you will need to re-authenticate your Trakt & TMDb accounts through Fen Light AM  if you are currently using them. ' \
+			text = 'This unfortunately means you will need to re-authenticate your Trakt & TMDb accounts through The Pirate AM  if you are currently using them. ' \
 			'My apologies, but with the previous keys being revoked, this is necessary.'
 			kodi_utils.ok_dialog(heading='Addon Credentials Reset', text=text)
 
 class CustomWindowsPrepare:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'CustomWindowsPrepare Service Starting')
+		kodi_utils.logger('The Pirate', 'CustomWindowsPrepare Service Starting')
 		from windows.base_window import FontUtils, ExtrasUtils
 		monitor, player = kodi_utils.kodi_monitor(), kodi_utils.kodi_player()
 		wait_for_abort, is_playing = monitor.waitForAbort, player.isPlayingVideo
@@ -105,11 +105,11 @@ class CustomWindowsPrepare:
 		except: pass
 		try: del player
 		except: pass
-		return kodi_utils.logger('Fen Light', 'CustomWindowsPrepare Service Finished')
+		return kodi_utils.logger('The Pirate', 'CustomWindowsPrepare Service Finished')
 
 class TraktMonitor:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'TraktMonitor Service Starting')
+		kodi_utils.logger('The Pirate', 'TraktMonitor Service Starting')
 		from apis.trakt_api import trakt_sync_activities
 		from modules.settings import trakt_user_active, trakt_sync_interval
 		monitor, player = kodi_utils.kodi_monitor(), kodi_utils.kodi_player()
@@ -122,27 +122,27 @@ class TraktMonitor:
 				next_update_string = update_string % sync_interval
 				if trakt_user_active: status = trakt_sync_activities()
 				else: status = 'no_auth'
-				if status == 'failed': kodi_utils.logger('Fen Light', trakt_service_string % ('Failed. Error from Trakt', next_update_string))
-				elif status == 'no_auth': kodi_utils.logger('Fen Light', trakt_service_string % ('Not Run. No Current Trakt Account', next_update_string))
+				if status == 'failed': kodi_utils.logger('The Pirate', trakt_service_string % ('Failed. Error from Trakt', next_update_string))
+				elif status == 'no_auth': kodi_utils.logger('The Pirate', trakt_service_string % ('Not Run. No Current Trakt Account', next_update_string))
 				else:
 					if status in ('success', 'no account'):
-						kodi_utils.logger('Fen Light', trakt_service_string % ('Success. %s' % trakt_success_line_dict[status], next_update_string))
+						kodi_utils.logger('The Pirate', trakt_service_string % ('Success. %s' % trakt_success_line_dict[status], next_update_string))
 					else:
-						kodi_utils.logger('Fen Light', trakt_service_string % ('Success. No Changes Needed', next_update_string))# 'not needed'
+						kodi_utils.logger('The Pirate', trakt_service_string % ('Success. No Changes Needed', next_update_string))# 'not needed'
 					if status == 'success' and get_setting('fenlight.trakt.refresh_widgets', 'false') == 'true':
 						kodi_utils.run_plugin({'mode': 'kodi_refresh'})
-			except Exception as e: kodi_utils.logger('Fen Light', trakt_service_string % ('Failed', 'The following Error Occured: %s' % str(e)))
+			except Exception as e: kodi_utils.logger('The Pirate', trakt_service_string % ('Failed', 'The following Error Occured: %s' % str(e)))
 			wait_for_abort(wait_time)
 		try: del monitor
 		except: pass
 		try: del player
 		except: pass
-		return kodi_utils.logger('Fen Light', 'TraktMonitor Service Finished')
+		return kodi_utils.logger('The Pirate', 'TraktMonitor Service Finished')
 
 class UpdateCheck:
 	def run(self):
 		if kodi_utils.get_property(firstrun_update_prop) == 'true': return
-		kodi_utils.logger('Fen Light', 'UpdateCheck Service Starting')
+		kodi_utils.logger('The Pirate', 'UpdateCheck Service Starting')
 		from modules.updater import update_check
 		from modules.settings import update_action, update_delay
 		end_pause = time() + update_delay()
@@ -158,11 +158,11 @@ class UpdateCheck:
 		except: pass
 		try: del player
 		except: pass
-		return kodi_utils.logger('Fen Light', 'UpdateCheck Service Finished')
+		return kodi_utils.logger('The Pirate', 'UpdateCheck Service Finished')
 
 class WidgetRefresher:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'WidgetRefresher Service Starting')
+		kodi_utils.logger('The Pirate', 'WidgetRefresher Service Starting')
 		from time import time
 		monitor, player = kodi_utils.kodi_monitor(), kodi_utils.kodi_player()
 		wait_for_abort, self.is_playing = monitor.waitForAbort, player.isPlayingVideo
@@ -177,7 +177,7 @@ class WidgetRefresher:
 					continue
 				if self.condition_check(): continue
 				if self.next_refresh < time():
-					kodi_utils.logger('Fen Light', 'WidgetRefresher Service - Widgets Refreshed')
+					kodi_utils.logger('The Pirate', 'WidgetRefresher Service - Widgets Refreshed')
 					kodi_utils.refresh_widgets()
 					self.set_next_refresh(time())
 			except: pass
@@ -185,7 +185,7 @@ class WidgetRefresher:
 		except: pass
 		try: del player
 		except: pass
-		return kodi_utils.logger('Fen Light', 'WidgetRefresher Service Finished')
+		return kodi_utils.logger('The Pirate', 'WidgetRefresher Service Finished')
 
 	def condition_check(self):
 		if not self.external(): return True
@@ -208,14 +208,14 @@ class WidgetRefresher:
 
 class AutoStart:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'AutoStart Service Starting')
+		kodi_utils.logger('The Pirate', 'AutoStart Service Starting')
 		from modules.settings import auto_start_fenlight
 		if auto_start_fenlight(): kodi_utils.run_addon()
-		return kodi_utils.logger('Fen Light', 'AutoStart Service Finished')
+		return kodi_utils.logger('The Pirate', 'AutoStart Service Finished')
 
 class AddonXMLCheck:
 	def run(self):
-		kodi_utils.logger('Fen Light', 'AddonXMLCheck Service Starting')
+		kodi_utils.logger('The Pirate', 'AddonXMLCheck Service Starting')
 		from xml.dom.minidom import parse as mdParse
 		self.addon_xml = kodi_utils.translate_path('special://home/addons/plugin.video.pirate/addon.xml')
 		self.root = mdParse(self.addon_xml)
@@ -223,7 +223,7 @@ class AddonXMLCheck:
 		self.check_property('reuse_language_invoker', 'reuselanguageinvoker')
 		self.check_property('addon_icon_choice', 'icon')
 		self.change_xml_file()
-		return kodi_utils.logger('Fen Light', 'AddonXMLCheck Service Finished')
+		return kodi_utils.logger('The Pirate', 'AddonXMLCheck Service Finished')
 
 	def check_property(self, setting, tag_name):
 		current_addon_setting = get_setting('fenlight.%s' % setting, None)
@@ -240,7 +240,7 @@ class AddonXMLCheck:
 		kodi_utils.notification('Refreshing Addon XML. Restarting Addons')
 		new_xml = str(self.root.toxml()).replace('<?xml version="1.0" ?>', '')
 		with open(self.addon_xml, 'w') as f: f.write(new_xml)
-		kodi_utils.logger('Fen Light', 'AddonXMLCheck Service - Change Detected. Restarting Addons')
+		kodi_utils.logger('The Pirate', 'AddonXMLCheck Service - Change Detected. Restarting Addons')
 		kodi_utils.execute_builtin('ActivateWindow(Home)', True)
 		kodi_utils.update_local_addons()
 		kodi_utils.disable_enable_addon()
@@ -280,6 +280,6 @@ class FenLightMonitor(Monitor):
 			kodi_utils.clear_property(pause_services_prop)
 			kodi_utils.logger('OnNotificationActions', 'UNPAUSING ThePirateStreaming Services Due to Device Awake')
 
-kodi_utils.logger('Fen Light', 'Main Monitor Service Starting')
+kodi_utils.logger('The Pirate', 'Main Monitor Service Starting')
 FenLightMonitor().waitForAbort()
-kodi_utils.logger('Fen Light', 'Main Monitor Service Finished')
+kodi_utils.logger('The Pirate', 'Main Monitor Service Finished')
